8e5f6f830682957a1971b9ce7faae84ed56d4814
capresti@gmail.com <capresti@gmail.com@63eb109e-e254-0410-a61e-ed0b8f8614f5>
2011-08-09 16:32:52 +0000

    - Added LuaInterface.net35 project, uses linked files to reference the files from the main LuaInterface project
    git-svn-id: http://luainterface.googlecode.com/svn/trunk@29 63eb109e-e254-0410-a61e-ed0b8f8614f5

---
+++ luainterface/src/LuaInterface.net35/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LuaInterface")]
+[assembly: AssemblyDescription("Bridge between the Lua runtime and the CLR")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LuaInterface")]
+[assembly: AssemblyCopyright("Copyright 2003-2008 Fabio Mascarenhas, Kevin Hester")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ccd6bdad-dddd-4f27-ba91-5a5f4e200239")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
