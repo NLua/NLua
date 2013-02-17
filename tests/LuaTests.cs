a0e1671c61bc5d1752b36e16207c4f820c7fd438
Vinicius Jarina <viniciusjarina@gmail.com>
2013-02-17 18:16:35 -0300

    Fixed test LuaBaseMethod on iOS.

--- tests/LuaTests.cs
+++ tests/LuaTests.cs
@@ -1368,17 +1368,55 @@ public class LuaTests
                         //Console.WriteLine("interface returned: "+a);
                 }
         }
-
+              
 
 #region LUA_BOILERPLATE_CLASS
         /*** This class is used to bind the .NET world with the Lua world, this boilerplate code is pratically the same, get values call Lua function return value back,
-         * this class is usually dynamic generated using System.Reflection.Emit, but this will not work on iOS. */
+        * this class is usually dynamic generated using System.Reflection.Emit, but this will not work on iOS. */
+
+        class LuaTestClassHandler: TestClass, ILuaGeneratedType 
+        {
+                        public LuaTable __luaInterface_luaTable;
+                        public Type[][] __luaInterface_returnTypes;
+
+                        public LuaTestClassHandler (LuaTable luaTable, Type[][] returnTypes)
+                        {
+                                __luaInterface_luaTable = luaTable;
+                                __luaInterface_returnTypes = returnTypes;
+                        }
+                        
+                        public LuaTable __luaInterface_getLuaTable ()
+                        {
+                                return __luaInterface_luaTable;
+                        }
+
+                        public override int overridableMethod(int x, int y)
+                        {
+                                object [] args = new object [] {
+                                        __luaInterface_luaTable,
+                                        x,
+                                        y
+                                };
+                                object [] inArgs = new object [] {
+                                        __luaInterface_luaTable,
+                                        x,
+                                        y
+                                };
+                                int [] outArgs = new int [] { };
+                                Type [] returnTypes = __luaInterface_returnTypes [0];
+                                LuaFunction function = LuaInterface.Method.LuaClassHelper.getTableFunction (__luaInterface_luaTable, "overridableMethod");
+                                object ret = LuaInterface.Method.LuaClassHelper.callFunction (function, args, returnTypes, inArgs, outArgs);
+                                return (int) ret;
+                        }
+        }
+
+
         class LuaITestClassHandler : ILuaGeneratedType, ITest
         {
                 public LuaTable __luaInterface_luaTable;
                 public Type[][] __luaInterface_returnTypes;
 
-                        public LuaITestClassHandler (LuaTable luaTable, Type[][] returnTypes)
+                public LuaITestClassHandler (LuaTable luaTable, Type[][] returnTypes)
                 {
                         __luaInterface_luaTable = luaTable;
                         __luaInterface_returnTypes = returnTypes;
@@ -1617,6 +1655,7 @@ public class LuaTests
         public void LuaTableBaseMethod ()
         {
                 using (Lua lua = new Lua () ) {
+                        lua.RegisterLuaClassType (typeof (TestClass), typeof (LuaTestClassHandler));
                         lua.DoString ("luanet.load_assembly('LuaInterfaceTest')");
                         lua.DoString ("TestClass=luanet.import_type('LuaInterfaceTest.Mock.TestClass')");
                         lua.DoString ("test={}");
