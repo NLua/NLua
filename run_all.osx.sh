b2431142af2fd0e5533aa83d9352a4f953200603
Vinicius Jarina <viniciusjarina@gmail.com>
2013-03-03 23:24:51 -0300

    Fixed run_all.osx.sh

--- run_all.osx.sh
+++ run_all.osx.sh
@@ -4,5 +4,6 @@ make -f Makefile.OSX
 xbuild KeraLua.sln /p:Configuration=Release
 cd ../../
 cp Core/KeraLua/external/lua/osx/lib/liblua51.dylib tests/liblua51.dylib
+xbuild LuaInterface.sln /p:Configuration=Release
 cd tests/
 nunit-console LuaInterfaceTest.dll
