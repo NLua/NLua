93cf684ede3d287d364320616cf4443c24e388d8
Vinicius Jarina <viniciusjarina@gmail.com>
2013-05-18 20:47:29 -0300

    Fixed the build.

--- Core/NLua/LuaLib/LuaLib.cs
+++ Core/NLua/LuaLib/LuaLib.cs
@@ -410,6 +410,11 @@ namespace NLua
 			return LuaCore.luaL_loadbuffer (luaState, buff, (uint)buff.Length, name);
 		}
 
+		public static int luaL_loadbuffer (LuaCore.lua_State luaState, byte [] buff, string name)
+		{
+			return LuaCore.luaL_loadbuffer (luaState, buff, (uint)buff.Length, name);
+		}
+
 		public static int luaL_loadfile (LuaCore.lua_State luaState, string filename)
 		{
 			return LuaCore.luaL_loadfile (luaState, filename);
