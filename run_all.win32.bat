751aa03dfe1bdfd38d69261983ae48104bcc6c8e
Vinicius Jarina <viniciusjarina@gmail.com>
2013-06-05 00:39:13 -0400

    Added KopiLua build configuration (only to avoid regressions on KopiLua).

--- run_all.win32.bat
+++ run_all.win32.bat
@@ -1,3 +1,4 @@
+erase tests\*.dll
 cd Core\KeraLua
 call Makefile.Win32.bat
 msbuild KeraLua.sln /p:Configuration=Release
@@ -6,3 +7,9 @@ xcopy Core\KeraLua\external\lua\win32\bin\*.dll tests\*.dll
 msbuild NLua.sln /p:Configuration=Release
 cd tests/
 nunit-console NLuaTest.dll /xml=$1
+cd ..
+erase tests\*.dll
+msbuild NLua.sln /p:Configuration=ReleaseKopiLua
+cd tests/
+nunit-console NLuaTest.dll /xml=$1
+
