dc6d80885a4fae14951332a54ac21e7dd2be1f5a
Vinicius Jarina <viniciusjarina@gmail.com>
2013-07-08 16:00:16 -0300

    Fixed 64bit build (Windows)

--- run_all.win.kopilua.bat
+++ run_all.win.kopilua.bat
@@ -1,4 +1,5 @@
 erase tests\*.dll
-msbuild NLua.sln /p:Configuration=ReleaseKopiLua
+msbuild NLua.sln /p:Configuration=ReleaseKopiLua /p:Platform="Any CPU"
 cd tests/
 nunit-console NLuaTest.dll /xml=$1
+cd ..
