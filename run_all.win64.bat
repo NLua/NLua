d53e873123e50e386573b5f3d4ee081a357f84ed
Vinicius Jarina <viniciusjarina@gmail.com>
2013-09-03 16:34:47 -0300

    Split NET35/NET40/NET45 builds

--- run_all.win64.bat
+++ run_all.win64.bat
@@ -1,10 +1,10 @@
 erase tests\*.dll
 cd Core\KeraLua
 call Makefile.Win64.bat
-msbuild KeraLua.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild KeraLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU"
 cd ..\..
 xcopy Core\KeraLua\external\lua\win64\bin64\*.dll tests\*.dll
-msbuild NLua.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild NLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU"
 cd tests/
 nunit-console NLuaTest.dll /xml=$1
 cd ..
