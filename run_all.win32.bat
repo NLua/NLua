297e9608254705ae217b39cc3b0fb0f7dbd72b66
Vinicius Jarina <viniciusjarina@gmail.com>
2014-06-29 17:25:53 -0300

    [build] Fixed https://github.com/NLua/NLua/issues/95 KopiLua directives was not defined.

--- run_all.win32.bat
+++ run_all.win32.bat
@@ -4,7 +4,7 @@ call Makefile.Win32.bat
 msbuild KeraLua.Net40.sln /p:Configuration=Release /p:DefineConstants=WSTRING /p:Platform="Any CPU"
 cd ..\..
 xcopy Core\KeraLua\external\lua\win32\bin\*.dll tests\*.dll
-msbuild NLua.Net40.sln /p:Configuration=Release /p:DefineConstants=WSTRING /p:Platform="Any CPU"
+msbuild NLua.Net40.sln /p:Configuration=Release /p:DefineConstants=WSTRING;LUA_CORE;CATCH_EXCEPTIONS /p:Platform="Any CPU"
 cd tests/
 nunit-console NLuaTest.dll /xml=$1
 cd ..
