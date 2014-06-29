a815509bfe0047005417a6fa4c41f0c793c47a4a
Vinicius Jarina <viniciusjarina@gmail.com>
2014-06-29 18:50:17 -0300

    [build] Better Android build.

--- run_all.win64.bat
+++ run_all.win64.bat
@@ -4,7 +4,7 @@ call Makefile.Win64.bat
 msbuild KeraLua.Net40.sln /p:Configuration=Release /p:DefineConstants=WSTRING /p:Platform="Any CPU"
 cd ..\..
 xcopy Core\KeraLua\external\lua\win64\bin64\*.dll tests\*.dll
-msbuild NLua.Net40.sln /p:Configuration=Release /p:DefineConstants=WSTRING;LUA_CORE;CATCH_EXCEPTIONS /p:Platform="Any CPU"
+msbuild NLua.Net40.sln /p:Configuration=Release /p:DefineConstants="WSTRING;LUA_CORE;CATCH_EXCEPTIONS" /p:Platform="Any CPU"
 cd tests/
 nunit-console NLuaTest.dll /xml=$1
 cd ..
