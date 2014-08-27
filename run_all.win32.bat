a3d9c22f185ae17ad8198a4a740566a53fc0f83a
Vinicius Jarina <viniciusjarina@gmail.com>
2014-08-27 02:20:53 -0300

    [KeraLua] Add DynamicLibraryPath to allow KeraLua to be used with x86 and amd64 versions of lua52.dll

--- run_all.win32.bat
+++ run_all.win32.bat
@@ -1,10 +1,10 @@
 erase tests\*.dll
 cd Core\KeraLua
 call Makefile.Win32.bat
-msbuild KeraLua.Net45.sln /p:Configuration=Release /p:DefineConstants=WSTRING /p:Platform="Any CPU"
+msbuild KeraLua.Net45.sln /p:Configuration=Release /p:DefineConstants=USE_DYNAMIC_DLL_REGISTER;WSTRING /p:Platform="Any CPU"
 cd ..\..
 xcopy Core\KeraLua\external\lua\win32\bin\*.dll tests\*.dll
-msbuild NLua.Net45.sln /p:Configuration=Release /p:DefineConstants="WSTRING;LUA_CORE;CATCH_EXCEPTIONS" /p:Platform="Any CPU"
+msbuild NLua.Net45.sln /p:Configuration=Release /p:DefineConstants="USE_DYNAMIC_DLL_REGISTER;WSTRING;LUA_CORE;CATCH_EXCEPTIONS" /p:Platform="Any CPU"
 cd tests/
 nunit-console-x86 NLuaTest.dll /xml=$1
 cd ..
