e533017954900414fd05d9edd342971b8e2c4ea3
Mervill <mervills.email@gmail.com>
2014-10-07 08:32:53 -0700

    Disable CLS Compliance in Unity3D.

---
+++ Core/NLua/Platform/CLSCompliantAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+#if UNITY_3D
+namespace NLua {
+	/// <summary>
+	/// Disables CLS Compliance in Unity3D.
+	/// </summary>
+	[Serializable]
+	[AttributeUsage(AttributeTargets.All)]
+	[ComVisible(true)]
+	public class CLSCompliantAttribute : Attribute {
+		private bool _compliant;
+		public CLSCompliantAttribute(bool isCompliant) { _compliant = isCompliant; }
+		public bool IsCompliant { get { return _compliant; } }
+	}
+}
+#endif
