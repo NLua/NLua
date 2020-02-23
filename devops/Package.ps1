a1ea6121ad4615250bbad9e539e802087c974d3c
Vinicius Jarina <viniciusjarina@gmail.com>
2020-02-22 23:00:04 -0800

    Added UWP support.

--- devops/Package.ps1
+++ devops/Package.ps1
@@ -1,10 +1,11 @@
-param ([string] $PackageId, [string] $NuSpecFile)
+param ([string] $PackageId)
 
 . .\devops\BuildFunctions.ps1
 
 $hash = Get-Current-Commit-Hash
 $releaseNotes = "Release: $($hash)"
+$NuSpecFile = $PackageId + '.nuspec'
 
 Update-NuSpec-Release-Notes $NuSpecFile $releaseNotes
 
-& nuget pack $NuSpecFile
+& nuget pack $NuSpecFile -NoPackageAnalysis
