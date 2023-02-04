94f2c180282d822189c3668a43f475df81856c3b
Vinicius Jarina <viniciusjarina@gmail.com>
2023-02-03 23:36:13 -0800

    Bump year + Release 1.6.2

--- tests/src/Properties/AssemblyInfo.cs
+++ tests/src/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLuaTest")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
